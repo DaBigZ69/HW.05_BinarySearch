d2b3a3cbe527a59b4c8d420c28a0d9bd6a073f52
a01666338-git <a01666338@tec.mx>
2025-09-04 10:36:28 -0600

    Update Lector-hw05.cpp

--- Lector-hw05.cpp
+++ Lector-hw05.cpp
@@ -1,5 +1,20 @@
 //Lector HW 05 Binary Search
 #include <iostream>
 #include <fstream>
-
+#include <sstream>
 using namespace std;
+
+int main(){
+  
+  ifstream imported_file;
+  imported_file.open("file_name.txt");
+  
+  
+  string imported_line;
+  while (getline(imported_file, imported_line)){
+    stringstream current_line(imported_line);
+    cout<<imported_line<<"\n";
+  }
+  imported_file.close();
+  
+}
