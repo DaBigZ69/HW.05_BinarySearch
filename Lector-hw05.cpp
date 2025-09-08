c5b166e482ce60f69c52467b762949b8b3640596
DaBigZ69 <zakuxyz9@gmail.com>
2025-09-08 10:20:12 -0600

    Update Lector-hw05.cpp
    Se agrega el cin que permite que el usuario ingrese el nombre de su archivo

--- Lector-hw05.cpp
+++ Lector-hw05.cpp
@@ -1,6 +1,12 @@
+//Lector HW 05 Binary Search
+/*
+Gerardo Alberto Mendoza Castillo A01666338
+Fabian Lopez Perez A01661836
+*/
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
 
 using namespace std;
 
@@ -31,7 +37,7 @@ vector<int> line_to_vector(string line) {
 
 int binary_search(vector<int> input_vector, int search_item) {
     int low_bound = 0, high_bound = input_vector.size() - 1, item_location = -2;
-    while (item_location == -2){
+    while (item_location == -2) {
         if (search_item == input_vector[(low_bound + high_bound) / 2])
             item_location = (low_bound + high_bound) / 2;
         else if (low_bound == high_bound)
@@ -45,7 +51,9 @@ int binary_search(vector<int> input_vector, int search_item) {
 }
 
 int main() {
-    string file_name = "test_binary_search.txt"; // Replace string with file name
+    string file_name; 
+    cout << "Inserte el nombre del archivo" << endl; // This allows that the user provides the name
+    cin >> file_name;
     vector<string> file_content = read_file(file_name);
     vector<int> L = line_to_vector(file_content[0]);
     int x = stoi(file_content[1]);
@@ -53,5 +61,6 @@ int main() {
     cout << slot << "\n";
     return 0;
 }
+
 
 
