5e8f538994380037c5763a64ab4760b86fe2fdd5
a01666338-git <a01666338@tec.mx>
2025-09-07 23:22:29 -0600

    Update Lector-hw05.cpp

--- Lector-hw05.cpp
+++ Lector-hw05.cpp
@@ -1,1 +1,57 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+vector<string> read_file(string file_name) {
+    ifstream imported_file(file_name);
+    if (!imported_file)
+        cout << "Error opening txt file" << endl;
+    vector<string> file_content;
+    string line;
+    while (getline(imported_file, line))
+        file_content.push_back(line);
+    imported_file.close();
+    return file_content;
+}
+
+vector<int> line_to_vector(string line) {
+    line.erase(0, 1).erase(line.size() - 1, 1);
+    int numeric_characters;
+    char text_characters;
+    vector<int> created_vector;
+    stringstream ss(line);
+    while (ss >> numeric_characters) {
+        created_vector.push_back(numeric_characters);
+        ss >> text_characters;
+    }
+    return created_vector;
+}
+
+int binary_search(vector<int> input_vector, int search_item) {
+    int low_bound = 0, high_bound = input_vector.size() - 1, item_location = -2;
+    while (item_location == -2){
+        if (search_item == input_vector[(low_bound + high_bound) / 2])
+            item_location = (low_bound + high_bound) / 2;
+        else if (low_bound == high_bound)
+            item_location = -1;
+        else if (search_item < input_vector[(low_bound + high_bound) / 2])
+            high_bound = (low_bound + high_bound) / 2 - 1;
+        else if (search_item > input_vector[(low_bound + high_bound) / 2])
+            low_bound = (low_bound + high_bound) / 2 + 1;
+    }
+    return item_location;
+}
+
+int main() {
+    string file_name = "test_binary_search.txt"; // Replace string with file name
+    vector<string> file_content = read_file(file_name);
+    vector<int> L = line_to_vector(file_content[0]);
+    int x = stoi(file_content[1]);
+    int slot = binary_search(L, x);
+    cout << slot << "\n";
+    return 0;
+}
+
 
